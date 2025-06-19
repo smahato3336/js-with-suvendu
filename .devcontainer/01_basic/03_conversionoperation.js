a886482991dff22249755c2566a6ae7d7b29452a
smahato3336 <smahato3336@gmail.com>
2025-06-19 01:52:11 +0000

    Updating Conversion Operations

--- .devcontainer/01_basic/03_conversionoperation.js
+++ .devcontainer/01_basic/03_conversionoperation.js
@@ -48,5 +48,47 @@ let booleanIsLoggedIn = Boolean(isPasswordGiven)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof(stringNumber));
+// console.log(stringNumber);
+// console.log(typeof(stringNumber));
+
+// ******************************************** Operations ****************************************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2 + 2); //Addition
+// console.log(2 - 2); //Substruction
+// console.log(2 * 2); //Multiplication
+// console.log(2 / 3); //Division
+// console.log(2 ** 3); //2 to the power 3
+// console.log(2 % 7); //Module (remainder)
+
+let str1 = "Suvendu"
+let str2 = "Mahatoo"
+
+let str3 = str1 + str2
+//console.log(str3);
+
+// console.log("1" + 2); //12  (if String first then remaining numbers converts into string)
+// console.log(1 + "2"); //12
+// console.log("1" + 2 + 2); //122
+// console.log(1 + 2 + "2"); //32 (if Number first then conversion first then show the string)
+//console.log((((1 + 2) * 3) / 3) % 2); //Use paranthesis in such case.
+
+// console.log(+true); //Converts boolean into number
+// console.log(+"") //not to use
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2 //not to use
+
+let gameCounter = 100
+gameCounter++; //suffix
+++gameCounter; //preffix
+gameCounter++;
+console.log(gameCounter);
+
+//Go to mdn and study post fix and prefix
+//link to study : 
+https://262.ecma-international.org/14.0/index.html?_gl=1*1rvh9v6*_ga*MjU1NDY0ODU0LjE3NTAyOTMwNjY.*_ga_TDCK4DWEPP*czE3NTAyOTMwNjUkbzEkZzEkdDE3NTAyOTMwODEkajQ0JGwwJGgw#sec-ecmascript-language-functions-and-classes
