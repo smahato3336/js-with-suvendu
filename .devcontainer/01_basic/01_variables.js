5e7a6be73245d688bab7156e69e5f086b69008ec
smahato3336 <smahato3336@gmail.com>
2025-06-19 00:40:38 +0000

    updated files

---
+++ .devcontainer/01_basic/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 1335566
+let accountEmail = "smahato@gmail.com"
+var accountPassword = "123456"
+accountCity = "Darjeeling"
+
+//accountId = 2// const cannot be change
+accountEmail = "ruma@google.com"
+accountPassword = "4321"
+accountCity = "jaipur"
+
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity]);
---
+++ .devcontainer/01_basic/02_datatypes.js
@@ -0,0 +1,36 @@
+"use strict"; //treat all JS code a newer version
+
+//alert("hello")
+//alert(3 + 3) // we are using node.js, not browser
+
+console.log (3
+    +
+    3) // code readability should be high
+console.log("Suvendu Mahatoo")
+
+/* Documentation
+MDC Documentation is not original documentation, this documentation is provide by Mozilla Organization.
+Original Documentation is https//tc39.es (ecma script)
+*/
+let name = "Suvendu Mahatoo"
+let age = 37
+let isloggedIn = true
+let state;
+
+//Premitive datatype//
+//number => 2 to power 53
+//null => standalone value
+//String => ""
+//Symbol => unique
+//bigint =>
+//boolean => true/false
+//undefined=> 
+
+//Non-premitive datatype//
+
+//object
+
+console.log(typeof "Suvendu Mahatoo");
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
