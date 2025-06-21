9c1cad4173a0ba392164d0df7b6778a6cb3da3ca
smahato3336 <smahato3336@gmail.com>
2025-06-21 17:16:04 +0000

    update strings

--- .devcontainer/01_basic/06_strings.js
+++ .devcontainer/01_basic/06_strings.js
@@ -62,5 +62,78 @@ let worldName = "Hello World"
 // console.log(typeof worldName);
 
 //blink: the <blink> element itself is removed from modern browsers, and blinking text is frowned upon by several accessibility standards. 
-console.log(worldName.blink('Hello'));
+//console.log(worldName.blink('Hello'));
 
+/*
+charAt()
+
+const sentence = "The quick brown fox jumps over the lazy dog.";
+const newIndex = 6;
+
+console.log(`The character at index ${newIndex} is ${sentence.charAt(newIndex)}`);
+
+const tennisPlayer = "Rafale Nadal";
+console.log(tennisPlayer.charAt());
+//No index provided, use 0 as default.
+console.log(tennisPlayer.charAt(0));
+console.log(tennisPlayer.charAt(1));
+console.log(tennisPlayer.charAt(2));
+console.log(tennisPlayer.charAt(3));
+console.log(tennisPlayer.charAt(4));
+console.log(tennisPlayer.charAt(5));
+console.log(tennisPlayer.charAt(6));
+console.log(tennisPlayer.charAt(7));
+console.log(tennisPlayer.charAt(133));
+*/
+/*
+charCodeAt()
+
+const newSentence = "Ruma Mahato"
+let secondIndex = 5
+console.log(`My wife's charCodeAt is ${newSentence.charCodeAt(secondIndex)} and compare with charAt ${newSentence.charAt(secondIndex)}`);
+
+let daughterName = "Sanvika Mahato"
+console.log(`The charCodeAt 'S' is ${daughterName.charCodeAt(0)} and charAt '0' is ${daughterName.charAt(0)}`);
+console.log(`The charCodeAt 'a' is ${daughterName.charCodeAt(1)} and charAt '1' is ${daughterName.charAt(1)}`);
+console.log(`The charCodeAt 'n' is ${daughterName.charCodeAt(2)} and charAt '2' is ${daughterName.charAt(2)}`);
+console.log(`The charCodeAt 'v' is ${daughterName.charCodeAt(3)} and charAt '3' is ${daughterName.charAt(3)}`);
+console.log(`The charCodeAt 'i' is ${daughterName.charCodeAt(4)} and charAt '4' is ${daughterName.charAt(4)}`);
+console.log(`The charCodeAt 'k' is ${daughterName.charCodeAt(5)} and charAt '5' is ${daughterName.charAt(5)}`);
+console.log(`The charCodeAt 'a' is ${daughterName.charCodeAt(6)} and charAt '6' is ${daughterName.charAt(6)}`);
+console.log(`The charCodeAt 'M' is ${daughterName.charCodeAt(8)} and charAt '7' is ${daughterName.charAt(8)}`);
+
+*/
+
+/*
+******************* concat() ******************
+The concat() method of Array instances is used to merge two or more arrays. 
+This method does not change the existing arrays, but instead returns a new array.
+
+let items = ["Bucket", "Mugg", "Coocking Utencils"];
+let newItems = ["Water Bottle", "Barby Doll", "Books"];
+
+const totalItems = items.concat(newItems);
+console.log(totalItems);
+*/
+
+/*
+------------includes()------------
+The includes() method of Array instances determines whether an array includes 
+a certain value among its entries, returning true or false as appropriate.
+
+let petCategory = ["Dog", "Cat", "Bird", "Rat"];
+console.log(petCategory.includes("Rat"));
+console.log(petCategory.includes("Bird"));
+console.log(petCategory.includes("Mat"));
+*/
+
+/*
+--------------endsWith()--------------
+The endsWith() method of String values determines whether a string ends with 
+the characters of this string, returning true or false as appropriate.
+
+
+let newSentense = "My wife is very intelligent and wise"
+console.log(newSentense.endsWith("wise"));
+console.log(newSentense.startsWith("My"));
+*/
