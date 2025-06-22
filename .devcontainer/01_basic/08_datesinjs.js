cf1f37fdd1c9ba8048766d7e9c4609420bbd9539
smahato3336 <smahato3336@gmail.com>
2025-06-22 17:44:05 +0000

    Dates in Js

---
+++ .devcontainer/01_basic/08_datesinjs.js
@@ -0,0 +1,26 @@
+
+/*
+// Dates
+
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toJSON()); //.Give Ctrl+space to see the options
+console.log(myDate.toLocaleString());
+console.log(myDate.toDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toTimeString());
+console.log(typeof myDate); // Date is a object in JavaScript.
+*/
+
+// let myCreatedDate = new Date(2025, 0, 23)
+
+//let myCreatedDate =  new Date(2025, 0, 23, 5, 3)
+
+let myCreatedDate =  new Date(2025, 0, 23, 5, 3)
+
+console.log(myCreatedDate.toDateString());
+console.log(myCreatedDate.toLocaleString());
+
+
+
+
