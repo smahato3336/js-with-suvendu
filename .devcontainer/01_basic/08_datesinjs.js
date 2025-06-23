6a76c8c69418e3eb95dc22d1fb4a70889ef7e26d
smahato3336 <smahato3336@gmail.com>
2025-06-23 23:32:34 +0000

    Dates and time update

--- .devcontainer/01_basic/08_datesinjs.js
+++ .devcontainer/01_basic/08_datesinjs.js
@@ -18,8 +18,28 @@ console.log(typeof myDate); // Date is a object in JavaScript.
 
 let myCreatedDate =  new Date(2025, 0, 23, 5, 3)
 
-console.log(myCreatedDate.toDateString());
-console.log(myCreatedDate.toLocaleString());
+//console.log(myCreatedDate.toDateString());
+//console.log(myCreatedDate.toLocaleString());
+
+let mmddyyyy = new Date("2023-01-16")
+//console.log(mmddyyyy.toLocaleDateString());
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(mmddyyyy.getTime());
+//console.log(Date.now()/1000); //date converts from miliseconds to seconds.
+//console.log(Math.floor(Date.now()/1000));// remove decimil number.
+ 
+let anotherDate = new Date()
+//console.log(anotherDate.getMonth() + 1); //It starts from 0-Jan, and get correct month you can add +1.
+//console.log(anotherDate.getDay() + 1); // It starts 0-Monday, and get correct day you can add +1.
+
+//console.log(`The month is ${anotherDate.getMonth() + 1} and date is ${anotherDate.getDay() + 1} and the time is ${anotherDate.getTime()}`);
+
+anotherDate.toLocaleDateString('default', {
+    weekday: "long"
+    })
+console.log(anotherDate);
 
 
 
