6feb48c3f0fd8a5e6f61f6818664af3ee57be690
smahato3336 <smahato3336@gmail.com>
2025-06-26 23:39:36 +0000

    Array updated

--- 02_Basic/02_Array.js
+++ 02_Basic/02_Array.js
@@ -10,12 +10,29 @@ const women_products = ["eye lyner", "Leap Stick", "Kajal", "Hair Dryer"]
 
 //Concat
 const category = mens_products.concat(women_products)
-console.log(category);
+//console.log(category);
 
 // Spread
 const newCategory = [...mens_products, ...women_products]
-console.log(newCategory);
+//console.log(newCategory);
 
+const another_array = [1, 2, 3, [4, 5, 6, [7, 8, 9, [10, 11, 12]]]]
 
+const usable_array = another_array.flat(Infinity) //.falt= he flat() method of Array instances creates a new array with all sub-array elements concatenated 
+// into it recursively up to the specified depth. You can use ".falt(Infinity) to go all inside depth together."
+console.log(usable_array);
+
+//First ask question is the element is Array? if false then convert the element into Array.
+
+console.log(Array.isArray("Suvendu"));
+console.log(Array.from("Suvendu"));
+
+//Array.of
+
+let score = 100
+let score1 = 200
+let score2 = 300
+
+console.log(Array.of(score, score1, score2)); //converting set of element into one array.
 
 
