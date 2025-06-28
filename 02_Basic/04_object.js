d6275311d72143af761a8fd2cf60241ac6f9629b
smahato3336 <smahato3336@gmail.com>
2025-06-28 23:55:19 +0000

    Updated object

--- 02_Basic/04_object.js
+++ 02_Basic/04_object.js
@@ -17,15 +17,40 @@ const regularUser = {
         }
     }
 }
-//console.log(regularUser.fullname.userfullname.firstname);
+//console.log(regularUser.fullname?.userfullname.firstname);
 
 const obj1 = {1: "a", 2: "b"}
 const  obj2 = {3: "a", 4: "b"}
 const obj4 = {5: "a", 6: "b"}
 
 //const obj3 = {obj1, obj2} 
-//const obj3 = Object.assign({} obj1, obj2, obj4) 
+//const obj3 = Object.assign({}, obj1, obj2, obj4) 
 //console.log(obj3);
  
-const obj3 = {...obj1, ...obj2}
+//const obj3 = {...obj1, ...obj2}
 //console.log(obj3);
+
+const users = [
+    {
+        id: 1,
+        email: "s@gmail.com"
+    },
+    {
+
+    }
+]
+
+users[1].email
+console.log(tinderUser);
+console.log(Object.keys(tinderUser));
+//The Object.keys() static method returns an array of a given object's own enumerable string-keyed property names.
+console.log(Object.values(tinderUser));
+//The Object.values() static method returns an array of a given object's own enumerable string-keyed property values.
+console.log(Object.entries(tinderUser));
+//The Object.entries() static method returns an array of a given object's own enumerable string-keyed property key-value pairs.
+console.log(tinderUser.hasOwnProperty('isLogged'));
+//he hasOwnProperty() method of Object instances returns a boolean indicating whether this object has the specified property as its own property (as opposed to inheriting it).
+
+
+
+
