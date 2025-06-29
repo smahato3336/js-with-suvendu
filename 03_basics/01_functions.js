b0d02fec0f761026d949d831d831d106c8f96788
smahato3336 <smahato3336@gmail.com>
2025-06-29 14:51:01 +0000

    functions in js

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,50 @@
+function myName() {
+console.log("S");
+console.log("u");
+console.log("v");
+console.log("e");
+console.log("n");
+console.log("d");
+console.log("u");
+}
+
+//myName()
+
+// function addTwoNumbers(number1, number2){
+//     console.log(number1 + number2);
+// }
+
+// addTwoNumbers(3, 5)
+// addTwoNumbers(3, "4")
+// addTwoNumbers(3, "a")
+// addTwoNumbers(3, null)
+//It can uses under a variable also.
+
+function addTwoNumbers(number1, number2){
+    
+    //let result = number1 + number2
+    //return result
+    //console.log("Ruma"); //after returning nothing print
+
+    return number1 + number2
+    
+}
+const result = addTwoNumbers(3, 5)
+
+//console.log("Result " + result);
+
+
+//Suvendu's Address
+
+function myAddress(homeaddress){
+    return permanentAddress
+}
+
+const permanentAddress = "Vill: Dengurpara, PO: Saguna, PS: Kalyani, Dist: Nadia, West Bengal, 741245"
+
+//console.log(permanentAddress);
+
+
+//console.log("Ruma's permanent address is " + permanentAddress);
+//console.table("Ruma's permanent address is " + permanentAddress);
+
