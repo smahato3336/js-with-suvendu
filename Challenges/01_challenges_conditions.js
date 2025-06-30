bde686f4f2acb78d59ba792edc79c36ee5bfae1a
smahato3336 <smahato3336@gmail.com>
2025-06-30 17:20:27 +0000

    05 challenges on conditions

---
+++ Challenges/01_challenges_conditions.js
@@ -0,0 +1,70 @@
+/*
+//Checking if a number is greater than another number;
+
+let num1 = 7;
+let num2 = 6;
+
+if (num1 > num2) {
+    console.log("num1 is greater than num2");
+}
+else {
+    console.log("num1 is not greater than num2");
+}
+
+*/
+/*
+//Checking if a string is equal to another string:
+
+let username = "Suvendu"
+let anotherUsername = "Ruma"
+
+if (username == anotherUsername) {
+    console.log("Pick another username");   
+}
+else {
+    console.log("Yes! this username is available");   
+}
+
+*/
+
+/*
+//Checking if a variable is a number or not:
+
+let score = 66;
+
+if (typeof score === "number") {
+    console.log("Yes, this is a number");
+    
+} else {
+    console.log("No, this is not a number");   
+}
+
+*/
+/*
+
+//Checking if a boolean value is true or false:
+
+let isTeaReady = false
+
+if (isTeaReady) {
+    console.log("Tea is Ready");
+    
+} else {
+    console.log("Tea is not Ready");
+    
+}
+*/
+
+/*
+// Checking if an array is empty or not:
+
+let items =["item1"];
+console.log(items.length);
+
+if (items.length === 0) {
+    console.log("Array is empty");
+    
+} else {
+    console.log("Array is not empty");
+}
+*/
