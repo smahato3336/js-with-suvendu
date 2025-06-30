22fc20281b468f027e2766f24c3d5c14f72ab9f8
smahato3336 <smahato3336@gmail.com>
2025-06-30 18:03:13 +0000

    Challenges on array

---
+++ Challenges/02_challenges_array.js
@@ -0,0 +1,76 @@
+/*
+01. Declare an array named 'teaFlavors' that contains the strings `"green tea"`,`"black tea"`, and `"oolong tea"`,
+    Access the first element of the array and store it in a variable named `firstTea`.
+    
+    let teaFlavors = ["green tea", "black tea", "oolong tea"];
+    
+    let firstTea = teaFlavors[0];
+    console.log(firstTea);
+*/
+
+/*
+02. Declare an array named 'cities' containing "London", "Tokyo", "Paris", and "New York", 
+    Access the third element in the array and store it in a variable named 'favoriteCity',
+    let cities = ["London", "Tokyo", "Paris", "New York"]
+    
+    const favoriteCity = cities[2];
+    console.log(favoriteCity);
+*/
+
+/*
+03. You have an array named 'teaTypes' containing "herbal tea", "white tea", and "masala chai", 
+    change the second element of the array to "jasmine tea"
+    
+    let teaTypes = ["herbal tea", "white tea", "masala chai"];
+    
+    teaTypes[1] = "jasmine tea";
+    console.log(teaTypes);
+*/
+
+/*
+04. Declare an array named 'citiesVisited' containing "Mumbai" and "Sydney",
+    Add "Berlin" to the array using the 'push' method.
+    
+    let citiesVisited = ["Mumbai", "Sydney"];
+    citiesVisited.push("Berlin");
+    console.log(citiesVisited);
+*/
+
+/*
+05. You have an array named 'teaOrders' with "chai", "iced tea", "matcha", and "earl grey", 
+    Remove the last element of the array using the 'pop' method and store it is a variable named 'lastOrder'.
+    
+    let teaOrders = ["chai", "iced tea", "matcha", "earl grey"];
+    
+    let lastOrder = teaOrders.pop();
+    console.log(teaOrders);
+*/
+
+/*
+06. You have an array named 'popularTeas' containing "green tea", "oolong tea", and "chai",
+    Create a soft copy of this array named 'softCopyTeas'.
+    
+    let popularTeas = ["green tea", "oolong tea", "chai"];
+    
+    let softCopyTeas = popularTeas;
+    
+    popularTeas.pop()
+    console.log(popularTeas);
+    console.log(softCopyTeas);
+*/
+
+/*
+07. You have an array named 'topCities' containing "Berlin", "Singapore", and "New York", 
+    Create a hard copy of this array named 'hardCopyCities'.
+    
+    let topCities = ["Berlin", "Singapore", "New York"];
+    
+    hardCopyCities = [...topCities];
+    topCities.pop()
+    console.log(hardCopyCities);
+    console.log(topCities);
+*/
+
+
+
+ 
