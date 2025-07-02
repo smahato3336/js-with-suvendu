53553b4ac45fc6f501311d328df290dc9c68c0ab
smahato3336 <smahato3336@gmail.com>
2025-07-02 00:00:18 +0000

    loop challenges

---
+++ Challenges/03_loopChallenges.js
@@ -0,0 +1,80 @@
+/*
+01. Write a 'while' loop that calculates the sum of all numbers from 1 to 5 and stores the result in a variable named 'sum',
+let sum = 0;
+let i = 1;
+
+while (i <= 5) {
+    sum +=i //sum = sum + i;
+    i++
+}
+console.log(sum);
+*/
+
+/*
+02. Write a 'while' loop that counts down from 5 to 1 and store the number in an array named 'countdown'.
+let countdown = [];
+let j = 5;
+while (j > 0) {
+    countdown.push(j);
+    j--
+}
+console.log(countdown);
+*/
+
+/*
+03. Write a 'do while' loop that prompts a user to enter their favorite tea type until they enter "stop", 
+    Store each tea type in an array named 'teaCollection'.
+    
+    let teaCollection = [];
+    let tea;
+    
+    do {
+        tea = prompt(`Enter your favorite tea (type "stop" to finish your order)`);
+        if (tea !== "stop") {
+            teaCollection.push(tea);
+        }
+    } while (tea !== "stop");
+    
+*/
+
+/*
+04. Write a 'do while' loop that adds numbers from 1 to 3 and store the result in a variable named 'total', 
+let total = 0
+k = 1
+do {
+    total += k
+    k++
+} while (k <= 3);
+console.log(total);
+
+*/
+
+/*
+05. Write a 'for' loop that multiplies each element in the array '[2, 4, 6]' by 2 and stores the results in a new
+    array named 'multipliedNumbers'.
+    
+    let multipliedNumbers = [];
+    let myarray = [2, 4, 6];
+    
+    for (let l = 0; l < myarray.length; l++) {
+        //takenumber = myarray[l] *2;
+        //multipliedNumbers.push(takenumber);
+        multipliedNumbers.push(myarray[l] *2);
+    }
+    
+    console.log(multipliedNumbers);
+*/
+
+/*
+06. Write a 'for' loop that lists all the cities in the array '["Paris", "New York", "Tokyo", "London"]' and 
+    stores each city in a new array named 'cityList',
+    let favoriteCity = ["Paris", "New York", "Tokyo", "London"];
+    let cityList = [];
+    
+    for(m = 0; m < favoriteCity.length; m++ ) {
+        cityList.push(favoriteCity[m]);
+    }
+    console.log(cityList);
+*/
+
+
