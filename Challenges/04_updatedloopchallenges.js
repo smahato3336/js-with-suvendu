f5fc1eeab892c01ed795f4a0df878cc0861d39a5
smahato3336 <smahato3336@gmail.com>
2025-07-04 17:33:22 +0000

    loop challenges update

--- Challenges/04_updatedloopchallenges.js
+++ Challenges/04_updatedloopchallenges.js
@@ -47,3 +47,70 @@
     console.log(smallNumbers);
 */
 
+/*
+04. Use a 'for-of' loop to iterate through the array ["Chai", "green tea", "herbal tea", "black tea"] and skip "herbal tea".
+    Store the other teas in an array named 'preferredTeas'.
+    
+    let teaTypes = ["chai", "green tea", "herbal tea", "black tea"];
+    let preferredTeas = [];
+    
+    for (const tea of teaTypes) {
+        if(tea === "herbal tea") {
+            continue;
+        }
+        preferredTeas.push(tea);
+    }
+    console.log(preferredTeas);
+    
+*/
+
+/*
+05. Use a 'for-in' loop to loop through an object containing city populations. 
+    Stop the loop when the population of "Berlin" is found and store all previous cities' 
+    populations in a new object named 'cityPopulations'.
+    let citiesPopulation = 
+    "London" : 8900000,
+    "New York" : 8400000,
+    "Berlin" : 3500000,
+    "Paris" : 2200000
+}
+
+let cityNewPopulations = {};
+
+//console.log(Object.keys(citiesPopulation));
+//console.log(Object.values(citiesPopulation));
+
+for (const city in citiesPopulation) {
+    if (city === "Berlin") {
+        break;
+    }
+    cityNewPopulations[city] = citiesPopulation[city];
+}
+console.log(cityNewPopulations);
+
+*/
+
+/*
+06. Use a 'for-in' loop to loop through an object containing city populations.
+    Skip any city with a poopulation below 3 million and store the rest in a new object named 'largeCities'.
+
+    let worldCities = {
+        "Sydney" : 5000000,
+        "Tokyo" : 9000000,
+        "Berlin" : 3500000,
+        "Paris" : 2200000
+    }
+    
+    let largeCities = {};
+    
+    for (const city in worldCities) {
+        if(worldCities[city] < 3000000) {
+            continue;
+        }
+        
+        largeCities[city] = worldCities[city];
+    }
+    console.log(largeCities);
+*/
+
+
