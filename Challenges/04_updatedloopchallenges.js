ce453c2aa69035476a4a0a7e8d4909ba3a7bc680
smahato3336 <smahato3336@gmail.com>
2025-07-06 17:14:45 +0000

    updated loop series

--- Challenges/04_updatedloopchallenges.js
+++ Challenges/04_updatedloopchallenges.js
@@ -113,4 +113,64 @@ console.log(cityNewPopulations);
     console.log(largeCities);
 */
 
+/*
+07. Write a 'forEach' loop that iterates through the array ["earl grey", "green tea", "chai", "oolong tea"], 
+    Stop the loop when "chai" is found, and store all previous tea types in an array named 'availableTeas'.
+    
+    let allTeaTypes = ["earl grey", "green tea", "chai", "oolong tea"];
+    let availableTeas = [];
+    
+    allTeaTypes.forEach(function(teas) {
+        if (teas === "chai") {
+            return;
+        }
+        availableTeas.push(teas);
+    })
+    console.log(availableTeas);
+*/
 
+/*
+08. Write a 'forEach' loop that iterates through the array ["Berlin", "Tokyo", "Sydney", "Paris"].
+    Skip "Sydney" and store the other cities in a new array named 'traveledCities.
+    let worldCities = ["Berlin", "Tokyo", "Sydney", "Paris"];
+    let traveledCities = [];
+    
+    worldCities.forEach((cities) => {
+        if(cities === "Sydney"){
+            return;
+        }
+        traveledCities.push(cities);
+    })
+    console.log(traveledCities);
+*/
+
+/*
+09. Write a 'for' loop that iterates through the array [2, 5, 7, 9], Skip the value of '7'
+    and multiply the rest by 2, Store the results in a new array named 'doubledNumbers'.
+    let numbers = [2, 5, 7, 9];
+    let doubledNumbers = [];
+    
+    for(let i = 0; i < numbers.length; i++) {
+        if(numbers[i] === 7) {
+            continue;
+        }
+        doubledNumbers.push(numbers[i] * 2);
+    }
+    console.log(doubledNumbers);
+*/
+
+/*
+10. Use a 'for-of' loop to iterate through the array ["Chai", "Green Tea", "Black Tea", "Jasmine Tea", "Herbal Tea"], and
+    stop when the length of the current tea name is greater than 10.
+    Store the teas iterated over in an array named 'shortTeas'.
+    let myTeas = ["Chai", "Green Tea", "Jasmine Tea", "Black Tea", "Herbal Tea"];
+    let shortTeas = [];
+    
+    for (const tea of myTeas) {
+        if(tea.length > 10) {
+            break;
+        }
+        shortTeas.push(tea);
+    }
+    console.log(shortTeas);
+*/
