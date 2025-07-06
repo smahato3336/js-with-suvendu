f835db84bc0f8330275b64147e911b60ee92fff8
smahato3336 <smahato3336@gmail.com>
2025-07-06 17:44:54 +0000

    update loop

--- Challenges/04_updatedloopchallenges.js
+++ Challenges/04_updatedloopchallenges.js
@@ -68,7 +68,7 @@
 05. Use a 'for-in' loop to loop through an object containing city populations. 
     Stop the loop when the population of "Berlin" is found and store all previous cities' 
     populations in a new object named 'cityPopulations'.
-    let citiesPopulation = 
+    let citiesPopulation = {
     "London" : 8900000,
     "New York" : 8400000,
     "Berlin" : 3500000,
