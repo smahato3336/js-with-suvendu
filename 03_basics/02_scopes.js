780f6f6e105f3670cc66b197c9ac3accc2b980a4
smahato3336 <smahato3336@gmail.com>
2025-07-26 17:07:10 +0000

    scopes in js

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,48 @@
+//Scopes starts with three keywords 1. Let, 2. Var, & 3. Const.
+
+//*********Global Scope**********
+// var c = 300
+let a = 300
+
+
+//**********Block Scope**********
+if(true){
+    let a = 10
+    const b = 20
+    var c = 30
+    console.log("INNER: ", a);
+    
+}
+
+console.log(a);
+// console.log(b);
+// console.log(c);
+
+function one(){
+    const username = "Suvendu"
+    function two(){
+        const website = "youtube"
+        console.log(username);
+        
+    }
+    // console.log(website);
+
+    // two()
+    
+}
+
+one();
+
+// In Nested Function, child function can access parent variable. Here two() can access one() variable.
+
+if (true) {
+    const username = "SUVENDU"
+    if (username === "SUVENDU") {
+        const website = " Youtube"
+        console.log(username + website);
+    }
+    // console.log(website);
+    
+}
+// console.log(username);
+
